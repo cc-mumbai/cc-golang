16917b1f0242bf72250e98625cce5e2faededa9f
Codecademy-Mumbai <84669396+Codecademy-Mumbai@users.noreply.github.com>
2021-05-25 22:57:25 +0530

    🎉 init commit

---
+++ 01-Introduction/intro.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// Note: only uncomment one main() function at a time when testing
+
+// Part One
+// func main() {
+// 	fmt.Println("Hello World")
+// }
+
+// Part Two
+func main() {
+  	fmt.Printf("Hi! My name is %s. I have lived in %s for %d years.\n", "Naruto", "The Leaf Village", 6)
+	fmt.Printf("Sasuke says I'm a scaredy cat , which is %t", true)
+}
