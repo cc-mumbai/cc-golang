1da75e50d4f7dc2f8b5dbd0c471a0524ce5e93d4
Hritesh <hriteshbebop@gmail.com>
2021-06-02 20:54:23 +0530

    🔁 add nested structs

--- 05-Structs/structs.go
+++ 05-Structs/structs.go
@@ -21,6 +21,11 @@ func describeNinja(n Ninja) string {
 	return desc
 }
 
+func describeGroup(g Anbu) string {
+	desc := fmt.Sprintf("This Ninja group has %d members. The newest ninja is %s %s. Accepting new ninjas: %t", len(g.ninjas), g.newNinja.FirstName, g.newNinja.LastName, g.spaceAvailable)
+	return desc
+}
+
 func main() {
 	genin := Ninja{
 		Level: 1, 
@@ -28,7 +33,23 @@ func main() {
 		LastName: "Uzumaki", 
 		Email: "naruto@konoha.com",
 	}
+
+	anbu := Ninja{
+		Level: 7, 
+		FirstName: "Kakashi", 
+		LastName: "Hatake", 
+		Email: "kakashi@konoha.com",
+	}
+
+	tsukuyomi := Anbu{
+		role: "admin",
+		ninjas: []Ninja{genin, anbu},
+		newNinja: anbu,
+		spaceAvailable: true,
+	}
+
 	fmt.Println(genin.Email)
 	fmt.Println(genin)
 	fmt.Println(describeNinja(genin))
+	fmt.Println(describeGroup(tsukuyomi))
 }
