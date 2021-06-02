d123aa05b67a1960419f78be81610d3f2d51d4cb
Hritesh <hriteshbebop@gmail.com>
2021-06-02 23:37:55 +0530

    👍 complete structs module

--- 05-Structs/structs.go
+++ 05-Structs/structs.go
@@ -22,6 +22,9 @@ func describeNinja(n Ninja) string {
 }
 
 func describeGroup(g Anbu) string {
+	if len(g.ninjas) > 2 {
+		g.spaceAvailable = false
+	}
 	desc := fmt.Sprintf("This Ninja group has %d members. The newest ninja is %s %s. Accepting new ninjas: %t", len(g.ninjas), g.newNinja.FirstName, g.newNinja.LastName, g.spaceAvailable)
 	return desc
 }
@@ -41,15 +44,26 @@ func main() {
 		Email: "kakashi@konoha.com",
 	}
 
+	og_anbu := Ninja{
+		Level: 9, 
+		FirstName: "Itachi", 
+		LastName: "Uchiha", 
+		Email: "itachi@akatsuki.com",
+	}
+
 	tsukuyomi := Anbu{
 		role: "admin",
-		ninjas: []Ninja{genin, anbu},
-		newNinja: anbu,
+		ninjas: []Ninja{genin, anbu, og_anbu},
+		newNinja: og_anbu,
 		spaceAvailable: true,
 	}
 
 	fmt.Println(genin.Email)
 	fmt.Println(genin)
 	fmt.Println(describeNinja(genin))
+	fmt.Println("-----")
 	fmt.Println(describeGroup(tsukuyomi))
+	fmt.Println("-----")
+	fmt.Println(tsukuyomi)   // Still True is shown hmm:( 
+	// Modifying a variable permanently requires more behaviour:)
 }
