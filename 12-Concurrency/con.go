be61991453f6f8bd7207a3b34f53e71a874401ed
Hritesh <hriteshbebop@gmail.com>
2021-06-08 12:31:52 +0530

    🌮 add concurrency module

---
+++ 12-Concurrency/con.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func handlePanic() {
+	if r := recover(); r != nil {
+		fmt.Println("PANIC")
+	}
+}
+
+func printStuff() {
+	defer wg.Done()
+	defer handlePanic()
+	for i := 0; i < 3; i++ {
+		fmt.Println(i)
+		time.Sleep(time.Millisecond * 300)
+	}
+}
+
+func say(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(time.Millisecond * 300)
+	}
+}
+
+func main() {
+	go say("Hello")
+	say("There")
+
+	wg.Add(1)
+	go printStuff()
+	wg.Wait()
+}
